fbd1c49ac45f01872ef9f809123a56a94f2b3d95
Hugo <149089365+humarroquin@users.noreply.github.com>
2025-11-17 20:03:06 -0600

    created and styled the footer for all pages

--- js/layout.js
+++ js/layout.js
@@ -4,6 +4,10 @@ async function loadLayout() {
   const headerResponse = await fetch("../components/header.html");
   const headerData = await headerResponse.text();
   document.querySelector("header").innerHTML = headerData;
+
+  const footerResponse = await fetch("../components/footer.html");
+  const footerData = await footerResponse.text();
+  document.querySelector("footer").innerHTML = footerData;
 }
 
 document.addEventListener("DOMContentLoaded", loadLayout);
