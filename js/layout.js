c37edb86b87d07bbfbacd24c8d15d6af71ae4436
Hugo <149089365+humarroquin@users.noreply.github.com>
2025-12-01 20:16:38 -0600

    fetched the data for the recipe cards

--- js/layout.js
+++ js/layout.js
@@ -16,3 +16,54 @@ async function loadLayout() {
 }
 
 document.addEventListener("DOMContentLoaded", loadLayout);
+
+// load recipes to page
+async function getData() {
+  const response = await fetch("./js/data.json");
+  if (!response.ok) {
+    throw new Error("Couldn't fetch data.");
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+async function createRecipeCards() {
+  const data = await getData();
+  const recipes = data.map((recipe) => {
+    return `
+     <li class="flex flex-column">
+            <img
+              src="${recipe.image.large}"
+              alt=""
+              class="mb-400"
+            />
+            <h2 class="text-preset-5 mb-300">${recipe.title}</h2>
+            <p class="mb-400">
+              ${recipe.overview}
+            </p>
+            <div class="recipe__details | flex flex-wrap mb-400">
+              <div class="flex">
+                <img src="./assets/images/icon-servings.svg" alt="" />
+                <p>Servings: <span>${recipe.servings}</span></p>
+              </div>
+              <div class="flex">
+                <img src="./assets/images/icon-prep-time.svg" alt="" />
+                <p>Prep: <span>10</span></p>
+              </div>
+              <div class="flex">
+                <img src="./assets/images/icon-cook-time.svg" alt="" />
+                <p>Cook: <span>0</span></p>
+              </div>
+            </div>
+            <button class="btn-outline | btn--primary btn--pill">
+              View recipe
+            </button>
+          </li>
+    `;
+  });
+  document.querySelector(".recipes__list-container").innerHTML =
+    recipes.join("");
+}
+
+createRecipeCards();
